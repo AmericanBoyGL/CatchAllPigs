8df92b0ca27211b8415803399e908f504c4812c7
AmericanBoyGL <grekagreka25@gmail.com>
2021-01-04 13:52:06 +0200

    update logic for pigs creator and escape

--- Assets/Scripts/Logic/PigEscape.cs
+++ Assets/Scripts/Logic/PigEscape.cs
@@ -1,11 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.EventSystems;
 
 namespace Logic
 {
     public class PigEscape : MonoBehaviour
     {
-        
+        private NavMeshAgent _agent;
+
+        private void Start()
+        {
+            _agent = GetComponent<NavMeshAgent>();
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                Vector3 dirToPlayer = transform.position - other.transform.position;
+
+                Vector3 newPos = transform.position + dirToPlayer;
+
+                _agent.SetDestination(newPos);
+            }
+        }
     }
 }
